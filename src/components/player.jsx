43f236b45046e06565cbbc64140f6993aadee606
Sanjyot <41758207+sanjyot242@users.noreply.github.com>
2023-12-26 12:36:03 -0800

    Developed Player Component and partial skeleton on Game Board

---
+++ src/components/player.jsx
@@ -0,0 +1,35 @@
+import { useState } from 'react';
+
+export default function Player({ initialName, symbol }) {
+  const [playerName, setPlayerName] = useState(initialName);
+  const [isEditing, setIsEditing] = useState(false);
+
+  function onEditClickHandler() {
+    setIsEditing((editing) => !editing);
+    setPlayerName(playerName);
+  }
+
+  function onChangeHandler(event) {
+    setPlayerName(event.target.value);
+  }
+  return (
+    <li>
+      <span className='player'>
+        {isEditing ? (
+          <input
+            type='text'
+            required
+            value={playerName}
+            onChange={onChangeHandler}></input>
+        ) : (
+          <span className='player-name'>{playerName}</span>
+        )}
+
+        <span className='player-symbol'>{symbol}</span>
+      </span>
+      <button onClick={onEditClickHandler}>
+        {isEditing ? 'Save' : 'Edit'}
+      </button>
+    </li>
+  );
+}
